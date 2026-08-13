7138b8e1904c376475ca756b65e7c6f5f52c4ca6
omid-sarkari <rors7371@gmail.com>
2026-08-14 01:52:02 +0330

    Add NestJS test controller and connect to Supabase successfully

--- server-nestjs/src/app.module.ts
+++ server-nestjs/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { TestController } from './test.controller'
+import { QuestionsModule } from './questions/questions.module'
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    QuestionsModule,
+  ],
   controllers: [TestController],
   providers: [],
 })
